94c557d229f49ea72254ce2502beb5c36f7f9e4c
balanviorel <balanviorel2@gmail.com>
2025-07-26 11:35:30 +0300

    Initial commit

---
+++ src/main/java/pixel/academy/setter_injection_app/common/MexicanChef.java
@@ -0,0 +1,14 @@
+package pixel.academy.setter_injection_app.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MexicanChef implements Chef {
+
+    @Override
+    public String getDailyRecipe() {
+
+        return " Preparare la pasta .";
+    }
+
+}
